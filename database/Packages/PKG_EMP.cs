98ea3d90166dda523d04ef775bd31f8e45368591
jaba777 <tskhovreba777@gmail.com>
2024-10-08 01:27:15 +0400

    Add project files.

---
+++ database/Packages/PKG_EMP.cs
@@ -0,0 +1,100 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using NpgsqlTypes;
+using database.Models;
+
+namespace database.Packages
+{
+    internal class PKG_EMP
+    {
+        public string connectionString;
+        public PKG_EMP() {
+            this.connectionString = "Host=localhost; Port=5432; Database=new_base; Username=postgres; Password=jaba123";
+        }
+
+        
+            public void Save_Employee(Employee employee)
+            {
+            
+            var conn = new NpgsqlConnection();
+            conn.ConnectionString = this.connectionString;
+            conn.Open();
+
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "INSERT INTO public.employee (first_name, last_name, position) VALUES (@FirstName, @LastName, @Position)";
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("@FirstName", NpgsqlDbType.Varchar).Value = employee.Name;
+            cmd.Parameters.Add("@LastName", NpgsqlDbType.Varchar).Value = employee.LastName;
+            cmd.Parameters.Add("@Position", NpgsqlDbType.Varchar).Value = employee.Position;
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            }
+
+        public void Update_Employee(Employee employee)
+        {
+            
+            var conn = new NpgsqlConnection();
+            conn.ConnectionString = this.connectionString;
+            conn.Open();    
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "UPDATE public.employee SET first_name = @FirstName, last_name = @LastName, position=@Position WHERE employee.id = @EmployeeId";
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("@FirstName",NpgsqlDbType.Varchar).Value=employee.Name;
+            cmd.Parameters.Add("@LastName",NpgsqlDbType.Varchar).Value=employee.LastName;
+            cmd.Parameters.Add("@Position",NpgsqlDbType.Varchar).Value=employee.Position;
+            cmd.Parameters.Add("@EmployeeId", NpgsqlDbType.Integer).Value = employee.ID;
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+        }
+
+        public void Delete_Employee(int employeeId) {
+            
+            var conn = new NpgsqlConnection();
+            conn.ConnectionString = this.connectionString;
+            conn.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "DELETE FROM public.employee WHERE employee.id=@EmployeeId";
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("@EmployeeId", NpgsqlDbType.Integer).Value = employeeId;
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public List<Employee> GetEmployee()
+        {
+            List<Employee> employees = new List<Employee>();
+            var conn = new NpgsqlConnection();
+            conn.ConnectionString = this.connectionString;
+            conn.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT * FROM public.employee";
+            cmd.CommandType = CommandType.Text;
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            while (reader.Read()) {
+                Employee emp = new Employee();
+                emp.ID = int.Parse(reader["id"].ToString());
+                emp.Name = reader["first_name"].ToString();
+                emp.Position = reader["position"].ToString();
+                emp.LastName = reader["last_name"].ToString();
+                employees.Add( emp );
+            }
+            //cmd.ExecuteNonQuery();
+            conn.Close();
+            return employees;
+        }
+
+    }
+}
---
+++ database/Program.cs
@@ -0,0 +1,53 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using NpgsqlTypes;
+using database.Packages;
+using database.Models;
+
+namespace database
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//add employee
+         //PKG_EMP package = new PKG_EMP();
+         //Employee employee = new Employee();
+         //employee.Name = Console.ReadLine();
+         //employee.LastName = Console.ReadLine();
+         //employee.Position = Console.ReadLine();
+
+            //package.Save_Employee(employee);
+
+            //Update Employee
+            //PKG_EMP package = new PKG_EMP();
+            //Employee employee = new Employee();
+            //employee.ID = int.Parse(Console.ReadLine());
+            //employee.Name = Console.ReadLine();
+            //employee.LastName = Console.ReadLine();
+            //employee.Position = Console.ReadLine();
+            //package.Update_Employee(employee);
+
+            //Delete Employee
+
+            //PKG_EMP package = new PKG_EMP();
+            //Employee employee = new Employee();
+            //int employeeId = int.Parse(Console.ReadLine());
+
+            //package.Delete_Employee(employeeId);
+
+            PKG_EMP package = new PKG_EMP();
+            List<Employee> employees = package.GetEmployee();
+
+            foreach (Employee employee in employees) { 
+            Console.WriteLine(employee.Name);
+            }
+
+
+        }
+    }
+}
